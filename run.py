385d69ba92bf451dcb9b82758d0501fbab0f336f
farukclk <39220698+farukclk@users.noreply.github.com>
2021-01-19 21:40:09 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,23 @@
+import os, sys
+if os.path.isfile(".log.txt") and os.path.isfile(".cap") and os.path.isfile(".mac"):
+ 
+  key=input("Onceki tarama devam ettirilsin mi? [y/n] : ")
+  if key.strip()=="y" or key.strip=="Y":
+   mac=open(".mac","r").read()
+   cap=open(".cap","r").read()
+  else:
+   mac=input("mac address: ")
+   cap=input("cap file   : ")
+   os.system("rm .log.txt")
+   open(".mac","w").write(mac)
+   open("cap","w").write(cap)
+else:
+   mac=input("mac address: ")
+   cap=input("cap file   : ")
+   os.system("rm .log.txt")
+   open(".mac","w").write(mac)
+   open("cap","w").write(cap)
+
+os.system("python3 set.py | aircrack-ng -w - -b " + mac + " " + cap)
+print(mac)
+print(cap)
