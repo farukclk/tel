fa104e5b71a51262979f5dfaafd7957589b205e6
farukclk <39220698+farukclk@users.noreply.github.com>
2021-01-20 00:26:36 +0300

    Update run.py

--- run.py
+++ run.py
@@ -8,16 +8,18 @@ if lo!="" and os.path.isfile(".cap") and os.path.isfile(".mac"):
    mac=open(".mac","r").read()
    cap=open(".cap","r").read()
   else:
+   os.system("rm .log.txt")
    mac=input("mac address: ")
    cap=input("cap file   : ")
    os.system("rm .log.txt")
    open(".mac","w").write(mac)
    open(".cap","w").write(cap)
 else:
+   os.system("rm .log.txt")
    mac=input("mac address: ")
    cap=input("cap file   : ")
    os.system("rm .log.txt")
    open(".mac","w").write(mac)
-   open("cap","w").write(cap)
+   open(".cap","w").write(cap)
 
 os.system("python3 set.py | aircrack-ng -w - -b " + mac + " " + cap)
