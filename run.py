c49425833fa0b9f0ca0c39f3cbe6f5e76d8e9dab
farukclk <39220698+farukclk@users.noreply.github.com>
2021-01-22 02:39:45 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,39 +1,36 @@
-from os import *
+from os import system, path, listdir
 
 system("sudo apt install aircrack-ng john -y ")
 
+def yaz(dosya, value):
+    d=open(dosya, "w")
+    d.write(value)
+    d.close()
+
 def control(f): #otrum ismi
-   file=f+"pass.txt"
+   file="." + f + "pass.txt"
    if path.isfile(file):
        print(f" '{file}' zaten kirildlmis, sifre:")
        system("cat "+ file)
        exit()
    
-def run():
-   s=input("yeni oturum ismi: ").strip()
-   if session=="":
-       print(" [!] hatali giris ")
-       exit()
-   control(s)
-   mac=input("mac address: ")
-   cap=input("cap file   : ")
-   os.system("rm .log.txt")
-   open(f".{s}mac","w").write(mac)
-   open(f"{s}.cap","w").write(cap)
-   system(f"python3 set.py | john --stdin --session={s} --stdout | aircrack-ng -w - -b {mac} -l .{s}pass.txt {cap}")
-   system(f"cat .{s}pass.txt >> passwords.txt")
 
+key=""
 for file in listdir("."):
     if file.endswith(".rec"):
-       key=input("Onceki tarama devam ettirilsin mi? [y/n] : ")
-
+       while True:
+           key=input("Onceki tarama devam ettirilsin mi? [y/n] : ").strip().lower()
+           if key=="y" or key=="n":
+               break
+       break
+print()
 if key.strip().lower()=="y":
    s=input("oturum ismi: ").strip()
    control(s)
-   if path.isfile(s)==False:
+   if path.isfile(s + ".rec")==False:
        print("[!] oturum bulunamadi")
        exit()
-   if not (f".{s}mac" in listdir(".") and f".{s}cap" in listdir(".")):
+   elif not (f".{s}mac" in listdir(".") and f".{s}cap" in listdir(".")):
        print(" [!] eksik dosya")
        exit()
    mac=open(f".{s}mac","r").read().strip()
@@ -41,5 +38,20 @@ if key.strip().lower()=="y":
    system(f"python3 set.py | john --restore={s} | aircrack-ng -w - -b {mac} -l .{s}pass.txt " + cap)
    system(f"cat .{s}pass.txt >> passwords.txt")
 else:
-   run()
+   s=input("yeni oturum ismi: ").strip()
+   control(s)
+   if s=="":
+       print(" [!] hatali giris ")
+       exit()
+   elif s + ".rec" in listdir("."):
+       print("{!] bu isimde oturum zaten mevcut")
+       exit()
+
+   mac=input("mac address: ").strip()
+   cap=input("cap file   : ").strip()
+   yaz(f".{s}mac", mac)
+   yaz(f".{s}cap", cap)
+   system(f"python3 set.py | john --stdin --session={s} --stdout | aircrack-ng -w - -b {mac} -l .{s}pass.txt {cap}")
+   system(f"cat .{s}pass.txt >> passwords.txt")
+
 
